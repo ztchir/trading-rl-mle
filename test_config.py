8a46934b0fe1d2fa0e8bf2aaece7cb2c726817a5
ztchir <ztchir@gmail.com>
2025-12-06 23:03:28 -0700

    Established file structure, git, environment.yaml and config files

---
+++ test_config.py
@@ -0,0 +1,9 @@
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+@hydra.main(version_base=None, config_path="configs", config_name="config")
+def my_app(cfg: DictConfig):
+  print(OmegaConf.to_yaml(cfg))
+  
+if __name__ == "__main__":
+    my_app()
